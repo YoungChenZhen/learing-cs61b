10ea20b205e27cad762be9af96210b91bdd44cba
YoengChenZhen <c736336982@outlook.com>
2022-08-08 11:10:32 +0800

    Finished the lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -6,7 +6,7 @@
 public class Collatz {
 
     /**
-     * Buggy implementation of nextNumber!
+     * Buggy implementation of nextNumber
      */
     public static int nextNumber(int n) {
         if (n % 2 == 0)
